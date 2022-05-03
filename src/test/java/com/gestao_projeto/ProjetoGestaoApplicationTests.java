39d23511952beb60da48f2dbdc4fcbde643116db
HyagoTrajano <hyagoeng02@gmail.com>
2022-05-03 16:15:45 -0300

    inicio-projeto

---
+++ src/test/java/com/gestao_projeto/ProjetoGestaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao_projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoGestaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
